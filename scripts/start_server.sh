0a24110c429ba6a17d85bfc75b8aff5efdb7bd2d
Gug007 <gurgen.abagyan@gmail.com>
2020-04-05 12:51:00 +0300

    feat docker run

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 cd /var/topmek
+sudo docker rmi $(docker images -a -q)
 sudo docker build -t gug007/topmek .
-sudo docker run -d -p 80:80 gug007/topmek
+sudo docker run --rm -d -p 80:80 gug007/topmek
