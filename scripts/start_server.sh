d434d20d10bdbfcd218b34c0c77420e0a02beae4
Gug007 <gurgen.abagyan@gmail.com>
2020-04-05 18:36:03 +0300

    test deployment

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
 cd /var/topmek
-sudo docker rm $(sudo docker ps -a -q) -f
-sudo docker rmi $(sudo docker images -a -q)
 sudo docker build -t gug007/topmek .
 sudo docker run --rm -d -p 80:80 gug007/topmek
