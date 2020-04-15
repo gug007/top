e3aa4723301b5036e1ce576620b4bf7f93178936
Gug007 <gurgen.abagyan@gmail.com>
2020-04-15 15:05:17 +0300

    fix start server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,5 +4,5 @@ sudo docker rm $(sudo docker ps -a -q) -f
 sudo docker rmi $(sudo docker images -a -q)
 # sudo docker build -t gug007/topmek .
 # sudo docker run --rm -d -p 80:80 gug007/topmek
-sudo docker-compose up --build
+docker-compose up --build
 
