a01b35968b6a4417f64251a396569c6619877c49
Gug007 <gurgen.abagyan@gmail.com>
2020-04-15 14:35:06 +0300

    init compose

--- scripts/codedeploy_agent.sh
+++ scripts/codedeploy_agent.sh
@@ -10,3 +10,6 @@ sudo mkdir /var/topmek
 sudo yum install docker -y
 sudo service docker start
 sudo usermod -a -G docker ec2-user
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
