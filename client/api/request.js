f22326a65fd2c7db52b3ded438435c12185eebf5
Gug007 <gurgen.abagyan@gmail.com>
2020-04-16 12:43:47 +0300

    add prod deployment

--- client/api/request.js
+++ client/api/request.js
@@ -1,11 +1,17 @@
 import fetch from "isomorphic-unfetch";
 import getConfig from "next/config";
 
+const isBrowser =
+  typeof window !== "undefined" && typeof window.document !== "undefined";
+
 const { publicRuntimeConfig } = getConfig();
 const { SERVER_HOST } = publicRuntimeConfig;
-const ROOT_API = `http://${SERVER_HOST || "localhost:4010"}/`;
+const ROOT_API = `http://${SERVER_HOST || "localhost:4010"}`;
 
 export default async endpoint => {
-  const res = await fetch(`${ROOT_API}${endpoint}`);
+  const ROOT = isBrowser
+    ? `${window.location.protocol}//api.${window.location.hostname}`
+    : ROOT_API;
+  const res = await fetch(`${ROOT}/${endpoint}`);
   return await res.json();
 };
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,4 +4,4 @@ sudo docker rm $(sudo docker ps -a -q) -f
 sudo docker rmi $(sudo docker images -a -q)
 # sudo docker build -t gug007/topmek .
 # sudo docker run --rm -d -p 80:80 gug007/topmek
-docker-compose up --build -d
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
