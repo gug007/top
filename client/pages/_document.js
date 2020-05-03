651364fda10dec29ebeaa827157ee7f0a318f6d1
Gug007 <gurgen.abagyan@gmail.com>
2020-05-03 18:05:10 +0300

    add analytic

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -3,8 +3,26 @@ import Document, { Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
 import theme from "../src/theme";
 
+const yandexAnalytic = `
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(62520667, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/62520667" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+`;
+
 class MyDocument extends Document {
   render() {
+    const isProduction = process.env.NODE_ENV === "production";
     return (
       <html lang="en">
         <Head>
@@ -29,19 +47,22 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          {isProduction && (
+            <div dangerouslySetInnerHTML={{ __html: yandexAnalytic }} />
+          )}
         </body>
       </html>
     );
   }
 }
 
-MyDocument.getInitialProps = async (ctx) => {
+MyDocument.getInitialProps = async ctx => {
   const sheets = new ServerStyleSheets();
   const originalRenderPage = ctx.renderPage;
 
   ctx.renderPage = () =>
     originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      enhanceApp: App => props => sheets.collect(<App {...props} />)
     });
 
   const initialProps = await Document.getInitialProps(ctx);
@@ -51,8 +72,8 @@ MyDocument.getInitialProps = async (ctx) => {
     // Styles fragment is rendered after the app and page rendering finish.
     styles: [
       ...React.Children.toArray(initialProps.styles),
-      sheets.getStyleElement(),
-    ],
+      sheets.getStyleElement()
+    ]
   };
 };
 
